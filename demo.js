668d4d3e53426f839d35f17c3968f08cee79e2ef
sgellar <1216181067@qq.com>
2020-03-05 19:42:05 +0800

    初始化

---
+++ demo.js
@@ -0,0 +1,69 @@
+import Http from "./src" ;
+var urlPrefix = "http://abit.tpddns.cn:9000/app/mock/44" ;
+var urls = {
+    submit:"/api/auth/submit",
+    fileVerify:"/api/auth/fileVerify",//导入数据页面文件校验
+    uploadPath:"/api/auth/uploadPath",//导入数据页面文件上传路径
+};
+var urls2 = {
+    submit:"/api/auth/submit",
+    fileVerify:"/api/auth/fileVerify",//导入数据页面文件校验
+    uploadPath:"/api/auth/uploadPath",//导入数据页面文件上传路径
+};
+var urls3 = {
+    prefixHttp:"https://parceljs.org/api/auth/submit",
+};
+var urlPrefix2 = "http://abit.tpddns.cn:9000/app/mock/48" ;
+var urls4 = {
+    collegePermissionControlList:"http://abit.tpddns.cn:9000/app/mock/42/api/auth/major",
+};
+
+
+document.getElementById("result").onclick = function () {
+    Http.registerApis(urlPrefix,urls); // 注册到window.Apis 作用域下，调用为window.Apis.delFile()
+    Http.registerApis(urlPrefix,urls,"namespace"); // 注册到window.Apis 作用域下，调用为window.Apis.delFile()
+    console.log(Http);
+};
+document.getElementById("result2").onclick = function () {
+    Http.registerApis(urlPrefix,urls3); // 注册到window.Apis 作用域下，调用为window.Apis.delFile()
+    console.log(Http);
+};
+document.getElementById("result3").onclick = function () {
+    Http.registerApis(urlPrefix,urls); // 注册到window.Apis 作用域下，调用为window.Apis.delFile()
+    Http.registerApis(urlPrefix2,urls4); // 注册到window.Apis 作用域下，调用为window.Apis.delFile()
+    Http.registerApis(urlPrefix,urls); // 注册到window.Apis 作用域下，调用为window.Apis.delFile()
+    Http.setMsg((msg,state)=>{
+        console.log(msg,state);
+    });
+    Http.registerApis(urlPrefix,urls3); // 注册到window.Apis 作用域下，调用为window.Apis.delFile()
+    Http.Apis.submit({ name:"1",age:"12" });
+    Http.Apis.collegePermissionControlList({ name:"1",age:"12" }).then(data=>{
+        console.log(data) ;
+        return data ;
+    }).then(Http.data).then(data=>{
+        console.log(data)
+    });
+    Http.Apis.submit({ name:"1",age:"12" }).then(data=>{
+        console.log(data) ;
+        return data ;
+    }).then(Http.bool).then(data=>{
+        console.log(data)
+    });
+};
+document.getElementById("result4").onclick = function () {
+    Http.registerApis(urlPrefix,urls); // 注册到window.Apis 作用域下，调用为window.Apis.delFile()
+    Http.setAxiosConfig({
+        headers:{
+            Authorization:"11"
+        }
+    });
+    Http.Apis.submit({ name:"1",age:"12" }).then(data=>{
+        console.log(data) ;
+        return data ;
+    }).then(Http.bool).then(data=>{
+        console.log(data)
+    });
+};
+
+
+
---
+++ src/index.js
@@ -0,0 +1,130 @@
+import Axios from "axios";
+const win = window || global ;
+win.Apis = {  } ;
+win._Apis = {  } ;
+
+
+let filterMsg = null ;
+let axiosConfig = null ;
+const HttpCode = { OK:"ok", FAil:"fail", };
+/**
+ * 处理后端返回{ state:"ok",msg:"删除成功" } 这种数据结构
+ * @param result
+ * @returns {boolean}
+ */
+const bool = function(result){
+    return result.state == HttpCode.OK ;
+} ;
+/**
+ * 处理后端返回的{ state:"ok",data:{ name:"sgellar",age:18 } } 这种数据结构
+ * @param result
+ * @returns {boolean|*}
+ */
+const data = function(result){
+    if(result.state == HttpCode.OK){
+        return result.data ;
+    }
+    return false ;
+};
+
+/***
+ * 编译路径参数，内部使用
+ * @param str
+ * @param obj
+ * @returns {*}
+ */
+function compile(str,obj){
+    Object.keys(obj).map(item=>{
+        let reg = new RegExp(`{${item}}`,"g") ;
+        str = str.replace(reg,obj[item]) ;
+    });
+    return str ;
+}
+
+/**
+ * 生成完整的URL
+ * @param prefix
+ * @param urls
+ */
+function generateURL(prefix,urls) {
+    const obj = {} ;
+    Object.keys(urls).forEach((key)=>{
+        let value = urls[key] ;
+        obj[key] = /^https:|^http:/.test(value)?value:prefix +  value ;
+    });
+    return obj ;
+}
+
+/**
+ * 设置axios的配置
+ * @param config
+ */
+const setAxiosConfig = function (config) {
+  axiosConfig = config ;
+};
+/**
+ * 获取axios的配置
+ * @param config
+ */
+const getAxiosConfig = function () {
+    return axiosConfig ;
+};
+
+const ajax = (url,data = {  })=>{
+    url = compile(url, data); // 将restful 里面的参数给替换掉
+    const config = getAxiosConfig() ;
+    return Axios({ method: 'post', url, data,...config }).then(response=>response.data);
+};
+const dataFilter = function(result){
+    if(result.msg){
+        typeof filterMsg == "function" && filterMsg(result.msg,result.state === HttpCode.OK);
+    }
+    return result ;
+} ;
+
+const setMsg = function (msg) {
+    if(typeof msg != "function"){
+        throw new Error("setMsg方法只能接受一个函数!");
+    }else{
+        filterMsg = msg ;
+    }
+};
+
+const cloneMap = function (source,target) {
+  Object.keys(target).map(key=>{
+      source[key] = target[key] ;
+  }) ;
+};
+
+function registerApis(prefixURL="",urls = {  },namespace) {
+    let _Apis = win._Apis ;
+    let Apis = win.Apis ;
+    const _urls = generateURL(prefixURL,urls);
+    if(namespace){ // 命名空间
+        if(!_Apis[namespace] ){
+            _Apis[namespace] = {};
+            Apis[namespace] = {};
+        }
+        _Apis = _Apis[namespace] ;
+        Apis = Apis[namespace] ;
+    }
+    _Apis = cloneMap(_Apis,_urls) ;
+    Object.keys(_urls).map((key)=>{
+        let value = _urls[key] ;
+        Apis[key] = function (data) {
+            return ajax(value,data).then(dataFilter) ;
+        } ;
+    });
+}
+
+export default {
+    registerApis,
+    data,
+    dataFilter,
+    bool,
+    Apis:win.Apis,
+    _Apis:win._Apis,
+    setMsg,
+    setAxiosConfig
+} ;
+
